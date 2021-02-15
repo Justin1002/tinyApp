bbde9f129c6f07ba5603deb2e7ff6af002a357f0
Justin Ly <jly3@ualberta.ca>
2021-02-15 20:36:05 +0000

    added urls_index view to /urls

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080;
 
 
 app.set('view engine', 'ejs')
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,6 +21,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase}
+  res.render("urls_index", templateVars);
+});
 
 
 app.listen(PORT, () => {

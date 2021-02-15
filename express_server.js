164e73b24648cc058e7b3dfeaade0ec9cec2318f
Justin Ly <jly3@ualberta.ca>
2021-02-15 22:39:52 +0000

    added delete functionality

--- express_server.js
+++ express_server.js
@@ -61,6 +61,12 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL
+  delete urlDatabase[shortURL]
+  res.redirect(`/urls/`);
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
